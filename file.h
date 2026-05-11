5df7fbcd3064bdf01c304c53776ac1f0c65741ea
Thaw Lin Htut <thawlinhtut09@gmail.com>
2026-05-11 12:33:13 +0700

    Refactor project and add modules

--- file.h
+++ file.h
@@ -1,7 +1,7 @@
 #ifndef FILE_H
 #define FILE_H
 
-#include "ui_system.h"
+#include "calendar.h"
 
 void saveToFile(struct Calendar* cal, const char* filename);
 void loadFromFile(struct Calendar* cal, const char* filename);
