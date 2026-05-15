c41219bf5e906aa1325899a59544af7333db72cb
Aye Theingi Myo <aye.thei@mail.kmutt.ac.th>
2026-05-15 22:49:03 +0700

    Adjust day formatting in display output

--- display.c
+++ display.c
@@ -36,11 +36,11 @@ void displayCalendar(struct Calendar* cal) {
         int eventCount = getEventCount(cal, day);
 
         if (eventCount == 0) {
-            printf("%3d  ", day);
+            printf("%2d  ", day);
         } else if (eventCount == 1) {
-            printf("%3d* ", day);
+            printf("%2d* ", day);
         } else {
-            printf("%3d*%d", day, eventCount);
+            printf("%2d*%d", day, eventCount);
         }
 
         // spacing fix for alignment
